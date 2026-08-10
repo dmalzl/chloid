71b6775dd10612c85ba29e9239a5887a9a54eabb
Daniel Malzl <52696584+dmalzl@users.noreply.github.com>
2026-08-10 15:06:54 +0200

    add patch build script

---
+++ build_patch.sh
@@ -0,0 +1,17 @@
+cat \
+  modules/clock_midi_modes.ini \
+  modules/gate_mode_page1.ini \
+  modules/gate_mode_page2.ini \
+  modules/gate_mode_page3.ini \
+  modules/gate_mode_page4.ini \
+  modules/gate_mode_envp.ini \
+  modules/gate_mode_clockp.ini
+  modules/cv_mode_page1.ini \
+  modules/cv_mode_page2.ini \
+  modules/cv_mode_page3.ini \
+  modules/cv_mode_page4.ini \
+  modules/cv_mode_envp.ini \
+  modules/cv_mode_clockp.ini \
+  modules/performance_mode_interface.ini \
+  algoquencers.ini \
+  > droid.ini
